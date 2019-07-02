3483d709c9b564e1d071f781aadb8bbf34822b53
Lukas Deutz <lukas.deutz@wirecard.com>
2019-07-02 17:01:26 +0200

    Remove '|| exit 1'

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -10,16 +10,16 @@ echo "run basic tests"
 php buildscripts/tests/basic-tests.php || true
 
 echo "build big html"
-RUBYOPT="-E utf-8" asciidoctor -b html5 -a systemtimestamp=$(date +%s) -a toc=left -a docinfo=shared -a icons=font index.adoc -o index.html &> /dev/null || exit 1
+RUBYOPT="-E utf-8" asciidoctor -b html5 -a systemtimestamp=$(date +%s) -a toc=left -a docinfo=shared -a icons=font index.adoc -o index.html &> /dev/null
 echo "create toc"
-node buildscripts/split-pages/create-toc.js &> /dev/null || exit 1
+node buildscripts/split-pages/create-toc.js &> /dev/null
 echo "create search index"
-node buildscripts/search/lunr-index-builder.js &> /dev/null || exit 1
+node buildscripts/search/lunr-index-builder.js &> /dev/null
 echo "build multipage docs"
-RUBYOPT="-E utf-8" asciidoctor -b multipage_html5 --failure-level=WARN -a linkcss -a systemtimestamp=$(date +%s) -a toc=left -a docinfo=shared -a icons=font -r asciidoctor-diagram -r ./buildscripts/asciidoc/multipage-html5-converter.rb index.adoc || exit 1
+RUBYOPT="-E utf-8" asciidoctor -b multipage_html5 --failure-level=WARN -a linkcss -a systemtimestamp=$(date +%s) -a toc=left -a docinfo=shared -a icons=font -r asciidoctor-diagram -r ./buildscripts/asciidoc/multipage-html5-converter.rb index.adoc
 
 # improve naming, CSS, etc.
-cp Home.html index.html &> /dev/null || exit 1
+cp Home.html index.html &> /dev/null
 # sed -i 's/normal\ normal\ 400\ normal\ 16px/normal normal 400 normal 15px/g' *.svg
 # sed -i 's/foreignObject width="[1-9][0-9]\+\.[0-9]\+" height="[1-9][0-9]\+\.[0-9]\+"/foreignObject width="100%" height="100%"/g' *.svg
 echo "post process svg files"
