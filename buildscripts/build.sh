da0add358d8ecf1e08dd2eba416814e27ceadd81
Lukas Deutz <lukas.deutz@wirecard.com>
2019-07-04 09:11:53 +0200

    Add time info to build.sh

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -6,7 +6,7 @@ function timed_log {
     echo "[$(date +'%T')] $1"
 }
 
-echo "set up environment"
+timed_log "set up environment"
 mkdir -p build/html
 cp -r errorpages/* css images js fonts resources build/html/
 
@@ -33,25 +33,25 @@ if ! diff -q --strip-trailing-cr "${checksum_new}" "${checksum_ref}"; then
     rm *.svg
 fi
 
-echo "run basic tests"
+timed_log "run basic tests"
 php buildscripts/tests/basic-tests.php || true
 
-echo "build big html"
+timed_log "build big html"
 RUBYOPT="-E utf-8" asciidoctor -b html5 -a systemtimestamp=$(date +%s) -a toc=left -a docinfo=shared -a icons=font index.adoc -o index.html &> /dev/null
-echo "create toc"
+timed_log "create toc"
 node buildscripts/split-pages/create-toc.js &> /dev/null
-echo "create search index"
+timed_log "create search index"
 node buildscripts/search/lunr-index-builder.js &> /dev/null
-echo "build multipage docs"
+timed_log "build multipage docs"
 RUBYOPT="-E utf-8" asciidoctor -b multipage_html5 --failure-level=WARN -a linkcss -a systemtimestamp=$(date +%s) -a toc=left -a docinfo=shared -a icons=font -r asciidoctor-diagram -r ./buildscripts/asciidoc/multipage-html5-converter.rb index.adoc
 
 # improve naming, CSS, etc.
 cp Home.html index.html &> /dev/null
 # sed -i 's/normal\ normal\ 400\ normal\ 16px/normal normal 400 normal 15px/g' *.svg
 # sed -i 's/foreignObject width="[1-9][0-9]\+\.[0-9]\+" height="[1-9][0-9]\+\.[0-9]\+"/foreignObject width="100%" height="100%"/g' *.svg
-echo "post process svg files"
+timed_log "post process svg files"
 sed -i 's/<foreignObject/<foreignObject style="overflow: visible;"/g' *.svg
 
 # create correct document structure
 mv *.svg toc.json searchIndex.json *.html build/html/
-echo "build complete."
+timed_log "build complete."
