9c34c311abedec6f37d31f515d587162fc1ae3cd
Lukas Deutz <lukas.deutz@wirecard.com>
2019-07-04 12:14:11 +0200

    Improve util/post-to-slack.py

--- buildscripts/util/post-to-slack.py
+++ buildscripts/util/post-to-slack.py
@@ -5,27 +5,38 @@ import os
 import json
 import requests
 
-if len(sys.argv) == 1:
-    print("Usage: {} <message in JSON>".format(sys.argv[0]))
-    sys.exit(1)
 
-message = sys.argv[1]
-URL = 'https://hooks.slack.com/services/'
-SLACK_TOKEN = os.getenv('SLACK_TOKEN')
-if SLACK_TOKEN is None:
-    print("##########################")
-    print("# No Slack Token in ENV! #")
-    print("# printing to stdout     #")
-    print("##########################")
-    print(message)
-    sys.exit(0)
+def post_to_slack(message):
+    URL = 'https://hooks.slack.com/services/'
+    SLACK_TOKEN = os.getenv('SLACK_TOKEN')
+    if SLACK_TOKEN is None:
+        print("##########################")
+        print("# No Slack Token in ENV! #")
+        print("# printing to stdout     #")
+        print("##########################")
+        print(message)
+        sys.exit(0)
 
-print(URL + SLACK_TOKEN)
-r = requests.post(URL + SLACK_TOKEN, data=message, headers={'Content-Type': 'application/json'})
+    print(URL + SLACK_TOKEN)
+    r = requests.post(URL + SLACK_TOKEN, data=message,
+                      headers={'Content-Type': 'application/json'})
 
-if r.status_code == 200:
-    sys.exit(0)
-else:
-    print("* Error sending message (status_code: {})".format(r.status_code))
-    print(r.text)
-    sys.exit(1)
+    if r.status_code == 200:
+        sys.exit(0)
+    else:
+        print("* Error sending message (status_code: {})".format(r.status_code))
+        print(r.text)
+        sys.exit(1)
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("Usage: {} <message in JSON>".format(sys.argv[0]))
+        sys.exit(1)
+
+    message = sys.argv[1]
+    post_to_slack(message)
+
+
+if __name__ == "__main__":
+    main()
