f2a6d08ffd8302c77a824cb2cba613e8c38e5c39
Lukas Deutz <lukas.deutz@wirecard.com>
2019-07-12 17:14:14 +0200

    Minor change

--- scripts/rename-samples.py
+++ scripts/rename-samples.py
@@ -207,7 +207,7 @@ def main():
         return
 
     for old, new in zip(files, processed_files):
-        if new is None:
+        if (new is None) or (old == new):
             continue
         if args.no_delete:
             copyfile(old, new)
