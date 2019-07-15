b5f7d12199f753e7dda367068529563d32899523
Lukas Deutz <lukas.deutz@wirecard.com>
2019-07-15 13:20:45 +0200

    Remove all trailing whitespaces

--- scripts/fix_unicode.sh
+++ scripts/fix_unicode.sh
@@ -11,6 +11,8 @@ for i in *.adoc; do
     progressBar
     sed -i "s/[”“]/\"/g;s/[‘’]/'/g" "$i" # remove 'smart' quotes
     sed -i 's/\xC2\xA0/ /g;s/\xef\xbb\xbf//g' "$i" # remove NoBreak Space and UTF-8 BOM
+    sed -i 's/[[:space:]]\+$//' "$i" # remove trailing whitespaces
+    # tr -s ' ' "$i" # squeeze multiple spaces
     (( count++ ))
 done
 
