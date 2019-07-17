f1f6462cf2acf2c4bd764dad76432bba4a8f9f12
Lukas Deutz <lukas.deutz@wirecard.com>
2019-07-17 15:19:37 +0200

    Update update_includes.sh

--- scripts/update_includes.sh
+++ scripts/update_includes.sh
@@ -3,9 +3,13 @@
 # REPORT="report-rename.json"
 # LENGTH="$(jq -r '.renames | .[] | .old + \" \" + .new' \"${REPORT}\" | wc -l)"
 # LENGTH=$(jq -r '.renames | .[] | .old + " " + .new' "${REPORT}" | wc -l)
+COUNT=1
+LENGTH=$(jq -r '.renames | .[] | .old + ";" + .new' report-rename.json | tr -d '\r' | wc -l)
 
 
 for line in $(jq -r '.renames | .[] | .old + ";" + .new' report-rename.json | tr -d '\r'); do
     IFS=';' read -ra array <<< "$line"
     sed -i "s:$(basename ${array[0]}):$(basename ${array[1]}):" *.adoc
+    printf "[%5s|%5s]\r" "$COUNT" "$LENGTH"
+    (( COUNT++ ))
 done
