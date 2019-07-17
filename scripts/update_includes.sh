32b61f06cc878744fae87b0bcfd2aa221065c483
Lukas Deutz <lukas.deutz@wirecard.com>
2019-07-17 15:31:39 +0200

    Improve update_includes.sh

--- scripts/update_includes.sh
+++ scripts/update_includes.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# REPORT="report-rename.json"
-# LENGTH="$(jq -r '.renames | .[] | .old + \" \" + .new' \"${REPORT}\" | wc -l)"
-# LENGTH=$(jq -r '.renames | .[] | .old + " " + .new' "${REPORT}" | wc -l)
 COUNT=1
 LENGTH=$(jq -r '.renames | .[] | .old + ";" + .new' report-rename.json | tr -d '\r' | wc -l)
 
-
 for line in $(jq -r '.renames | .[] | .old + ";" + .new' report-rename.json | tr -d '\r'); do
     IFS=';' read -ra array <<< "$line"
-    sed -i "s:$(basename ${array[0]}):$(basename ${array[1]}):" *.adoc
+    files="$(grep -lF ${array[0]} *.adoc)"
+    sed -i "s:$(basename ${array[0]}):$(basename ${array[1]}):" "$files"
     printf "[%5s|%5s]\r" "$COUNT" "$LENGTH"
     (( COUNT++ ))
 done
