04720a84d0f0c10d8a9e8369f1670cefac5cffbc
Herbert Knapp <herbert.knapp@wirecard.com>
2019-09-04 12:45:57 +0200

    Keep scroll pos on resize

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -74,25 +74,32 @@ function documentReady() {
   }
   var scrollTimer;
   var scrollDelay = 200;
+  var contentHeight = 0;
   $(window).on('scroll', function () {
     window.clearTimeout(scrollTimer);
     scrollTimer = setTimeout(function () {
+      contentHeight = $('#content').height;
       window.requestAnimationFrame(highlightTOC);
     }, scrollDelay);
   });
 
-  /*
+  
   var resizeTimer;
   var resizeDelay = 1000;
   $(window).on('resize', function () {
     if (getUrlHash()) {
       window.cancelIdleCallback(resizeTimer);
+      const oldHeight = contentHeight;
       resizeTimer = requestIdleCallback(function () {
-        window.location.href = '#' + getUrlHash();
+  //      window.location.href = '#' + getUrlHash();
+        const newHeight = $('#content').height;
+        const resizeFactor = oldHeight / newHeight;
+        const scrollPos = $('#content').scrollTop();
+        $('#content').scrollTop(scrollPos * resizeFactor);
       }, { timeout: resizeDelay });
     }
   });
-  */
+  
   // clipboard functions
   $(function () {
     var pre = document.getElementsByTagName('pre');
