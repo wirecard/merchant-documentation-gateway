f4ebcc748b31b6dc91bfe66f6bad5fe24d18b39c
Herbert Knapp <herbert.knapp@wirecard.com>
2019-10-09 13:05:46 +0200

    Add executable flag to build script

--- buildscripts/build-wrapper.sh
+++ buildscripts/build-wrapper.sh
@@ -7,7 +7,7 @@ TIMEOUT_MINUTES=30
 
 ## END CONFIG ##
 
-
+chmod +x ${BUILDSCRIPT}
 ${BUILDSCRIPT} &
 MAIN_PID=$!
 
@@ -16,7 +16,7 @@ MINUTES_COUNTER=0
 
 while kill -0 ${MAIN_PID} >&/dev/null; do
   echo -n -e " \b"
-  if [[] ${MINUTES_COUNTER} == ${TIMEOUT_MINUTES} ]]; then
+  if [[ ${MINUTES_COUNTER} == ${TIMEOUT_MINUTES} ]]; then
     echo -e "\n"
     echo -e "${RED}Wrapper script: timeout reached: ${TIMEOUT_MINUTES} minutes"
     exit 1
