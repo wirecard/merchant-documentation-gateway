7a4f34c6c94af8c6f17548512236d3f23b183d43
Lukas Deutz <lukas.deutz@wirecard.com>
2019-11-21 09:20:56 +0100

    Add specific_install of asciidoctor-diagram to docker/run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -13,6 +13,10 @@ fi
 
 npm install
 gem install bundler && bundle install
+# Workaround for new asciidoctor-diagram version
+gem install specific_install && \
+    gem specific_install -l https://github.com/asciidoctor/asciidoctor-diagram \
+                         -r 34a54807d287b8531ac8d4d33c2b5988788109fc
 pip3 install -r requirements.txt
 pip3 install requests
 
