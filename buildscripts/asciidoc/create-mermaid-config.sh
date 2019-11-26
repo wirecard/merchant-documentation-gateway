8890569b241bded8dccc185b00a99f9cd9272c40
Lukas Deutz <lukas.deutz@wirecard.com>
2019-11-26 09:55:02 +0100

    Use new font for mermaid: Lato

--- buildscripts/asciidoc/create-mermaid-config.sh
+++ buildscripts/asciidoc/create-mermaid-config.sh
@@ -6,6 +6,6 @@ mkdir -p config
 echo -n '{
     "theme": null,
     "themeCSS": "' > "${CONFIG_FILE}"
-cat css/wirecard-font-base64.css >> "${CONFIG_FILE}"
+# cat css/wirecard-font-base64.css >> "${CONFIG_FILE}"
 cat css/mermaid.css | sed 's/"/\\"/g' | tr -d '\n' | tr -s ' ' >> "${CONFIG_FILE}"
 echo '"}' >> "${CONFIG_FILE}"
