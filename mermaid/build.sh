e4f463ef5859cd696344ab5bf2e6deccd49931ea
Lukas Deutz <lukas.deutz@wirecard.com>
2020-05-13 08:49:04 +0200

    Improve mermaid/build.sh

--- mermaid/build.sh
+++ mermaid/build.sh
@@ -3,7 +3,10 @@
 # This script will build the Mermaid sources (ending in *.mmd BUT NOT *.orig.mmd) and copy
 # them to the images folder.
 
-for mmd in $(*.mmd | grep -v "*.orig.mmd"); do
+set -euo pipefail
+
+for mmd in $(ls *.mmd | grep -v -E ".*.orig.mmd"); do
+
     mmdc -i "${mmd}" -o "${mmd%.mmd}.svg"
 done
 
